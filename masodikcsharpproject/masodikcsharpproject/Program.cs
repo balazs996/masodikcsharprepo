d7ca5c54b13918f0f964ac9a42296d7cb3904cea
balazs996 <balazs19960424@gmail.com>
2019-11-12 15:08:35 +0100

    ismét kommit

--- masodikcsharpproject/masodikcsharpproject/Program.cs
+++ masodikcsharpproject/masodikcsharpproject/Program.cs
@@ -10,6 +10,79 @@ namespace masodikcsharpproject
     {
         static void Main(string[] args)
         {
+            /* Console.WriteLine("1-től 100-ig azok a számok melyek valódi osztóinak összege osztható 7-el");
+             for (int i = 1; i < 101; i++)
+             {
+                 if (sumSeven(i))
+                 {
+                     Console.WriteLine($"{i} valódi osztóinak száma osztható 7-tel.");
+                 }            
+             }*/
+            Console.WriteLine("2-től 100-ig");
+
+            for (int i = 2; i < 101; i++)
+            {
+                if (primszamE(i))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+
+            Console.ReadKey(true);
+            
         }
+
+        static bool sumSeven(int num)
+        {
+            int sum = 0;
+            bool wSeven = true;
+
+            for (int i = 2; i < num; i++)
+            {
+                if (num % i == 0)
+                {
+                    sum += i;
+                }
+            }
+
+            if (sum % 7 != 0)
+            {
+                wSeven = false;
+            }
+
+            return wSeven;
+        }    
+
+        static bool primszamE(int num)
+        {
+            bool primE = true;
+            int hatar = (int)Math.Floor(Math.Sqrt(num)) + 1;
+
+            for (int i = 2; i < hatar; i++)
+            {
+                if (num % i == 0)
+                {
+                    primE = false;
+                    break;
+                }
+            }
+
+            return primE;
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
     }
 }
